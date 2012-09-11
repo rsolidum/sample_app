3379b13a400cab57bcab36277f16d0fccd163d5f
rsolidum <rsolidum@gmail.com>
2012-09-11 00:55:37 -0400

    make basic User model (including secure passwords)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 SampleApp::Application.routes.draw do
+  get "user/new"
+
   get "users/new"
 
   root to: 'static_pages#home'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,8 +35,27 @@ describe "Static pages" do
   describe "Contact" do
     before { visit contact_path }
 
-    it { should have_selector('h1', :text => 'About Us') }
+    it { should have_selector('h1', :text => 'Contact') }
 
     it { should have_selector('title', text: full_title('')) }
+  end
+
+  describe "Static Pages" do
+    it "should have the right links" do
+      visit root_path
+      click_link "About"
+      page.should have_selector 'title', text: full_title('About Us')
+      click_link "Help"
+      page.should have_selector 'title', text: full_title('Help')
+      click_link "Contact"
+      page.should have_selector 'title', text: full_title('Contact')
+      click_link "Home"
+      click_link 'Sign up now!'
+      page.should have_selector 'title', text: full_title('Sign up')
+      click_link "Home"
+      page.should have_selector 'title', text: full_title('')
+      click_link "sample app"
+      page.should have_selector 'title', text: full_title('')
+    end
   end
 end
